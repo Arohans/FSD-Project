540d0883a41ebb6996ad05b6d49619b0a5059a4a
Arohans <104666887+Arohans@users.noreply.github.com>
2022-12-10 20:35:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,3 +48,21 @@ app.get("/secret", isLoggedIn, function (req, res) {
 app.get("/register", function (req, res) {
 	res.render("register");
 });
+
+// Handling user signup
+app.post("/register", function (req, res) {
+	var username = req.body.username
+	var password = req.body.password
+	User.register(new User({ username: username }),
+			password, function (err, user) {
+		if (err) {
+			console.log(err);
+			return res.render("register");
+		}
+
+		passport.authenticate("local")(
+			req, res, function () {
+			res.render("secret");
+		});
+	});
+});
